e07862fde29d6ca46d6ca71dd0193297ccf2fe49
Mark Graham <markgraham924@gmail.com>
2025-02-15 19:47:46 +0000

    Update deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -6,23 +6,16 @@ echo "Deploying Frontend..."
 # cd /home/runneruser/obw-checking
 git pull
 
-# Switch to the frontend folder.
-cd frontend
-
 # Install dependencies.
 npm install
 
 # Build the project using Vite (builds into the 'dist' folder).
 npm run build
 
-# Ensure the live directory exists and has correct permissions.
-sudo mkdir -p /var/www/frontend
-sudo chown runneruser:runneruser /var/www/frontend
-
 # Copy the build output from the 'dist' folder to the live directory.
 cp -r dist/* /var/www/frontend/
 
 # Reload nginx to ensure it serves the latest files.
-sudo systemctl reload nginx
+systemctl reload nginx
 
 echo "Frontend deployed successfully."
